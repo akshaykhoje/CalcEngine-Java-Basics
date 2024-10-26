a7a5b1112ffffcaceffba1412d304631c1562ec9
akshaykhoje <code.akshaykhoje@gmail.com>
2024-10-26 17:25:33 +0530

    Interactive mode added - Functional!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
@@ -13,7 +15,9 @@ public class Main {
             }
             for (double currentResult : results)
                     System.out.println(currentResult);
-        }  else if (args.length == 3) {
+        } else if (args.length == 1 && args[0].equals("interactive")){
+            executeInteractively();
+        } else if (args.length == 3) {
             handleCommandLine(args);
         }
     }
@@ -47,11 +51,50 @@ public class Main {
         return result;
     }
 
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        String output = leftVal + " " + opCode + " " + rightVal + " " + " = " + result;
+        System.out.println(output);
+    }
+
     private static void handleCommandLine(String[] args) {
         char opCode = args[0].charAt(0);
         double leftVal = Double.parseDouble(args[1]);
         double rightVal = Double.parseDouble(args[2]);
         double result = execute(opCode, leftVal, rightVal);
         System.out.println(result);
+    }
+
+    private static void executeInteractively() {
+        System.out.println("Enter an operand followed by two numbers e.g. \"a 3 6\" to perform \"3 + 6\"");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        if (parts.length != 3) {
+            System.out.println("Invalid input");
+        } else
+            performOperations(parts);
+    }
+
+    private static void performOperations(String[] arr) {
+        char opCode = opCodeFromString(arr[0]);
+        double leftVal = operandToDouble(arr[1]);
+        double rightVal = operandToDouble(arr[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    static char opCodeFromString(String operation) {
+        char opCode = ' ';
+        if (operation.length() == 1) {
+            opCode = operation.charAt(0);
+        } else {
+            System.out.println("Invalid opCode.\nThe available opCodes are 'a', 's', 'm', 'd'");
+            System.exit(0);
+        }
+        return opCode;
+    }
+
+    static double operandToDouble(String operand) {
+        return Double.parseDouble((operand));
     }
 }
